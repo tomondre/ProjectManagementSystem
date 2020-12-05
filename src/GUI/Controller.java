f9eb79621453cf1b65f02bf5998a1ee53f02053b
Khaled Hammoun <kh.hammoun@gmail.com>
2020-12-05 15:12:04 +0100

    Working process continued.

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,3 +1,4 @@
+
 package GUI;
 
 import FileAdapter.SystemAdapter;
@@ -5,122 +6,172 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import model.Project;
-import model.ProjectList;
 import model.ProjectManagmentSystem;
 
-import java.util.ArrayList;
-import java.util.Collection;
+public class Controller {
 
-public class Controller
-{
+  @FXML
+  private MenuItem exitMenuItem;
 
-  @FXML private MenuItem exitMenuItem;
+  @FXML
+  private RadioMenuItem ongoingProjects;
 
-  @FXML private RadioMenuItem ongoingProjects;
+  @FXML
+  private ToggleGroup projects;
 
-  @FXML private ToggleGroup projects;
+  @FXML
+  private RadioMenuItem archivedProjects;
 
-  @FXML private RadioMenuItem archivedProjects;
+  @FXML
+  private MenuItem aboutMenuItem;
 
-  @FXML private MenuItem aboutMenuItem;
+  @FXML
+  private Tab projectsTab;
 
-  @FXML private Tab projectsTab;
+  @FXML
+  private ListView<String> projectsListView;
 
-  @FXML private ListView<String> projectsListView;
+  @FXML
+  private TextField projectNameTextField;
 
-  @FXML private TextField projectNameTextField;
+  @FXML
+  private ComboBox<?> projectStatusComboBox;
 
-  @FXML private ComboBox<Project> projectStatusComboBox;
+  @FXML
+  private Button addProjectButton;
 
-  @FXML private Button addProjectButton;
+  @FXML
+  private Button editProjectButton;
 
-  @FXML private Button editProjectButton;
+  @FXML
+  private ComboBox<?> availableEmployeeComboBox;
 
-  @FXML private ComboBox<?> availableEmployeeComboBox;
+  @FXML
+  private Button assignEmployeeButton;
 
-  @FXML private Button assignEmployeeButton;
+  @FXML
+  private Button removeEmployeeButton;
 
-  @FXML private Button removeEmployeeButton;
+  @FXML
+  private Button saveProjectButton;
 
-  @FXML private Button saveProjectButton;
+  @FXML
+  private ListView<?> teamMembersListView;
 
-  @FXML private TextArea teamMembersTextArea;
+  @FXML
+  private Tab requirementsTab;
 
-  @FXML private Tab requirementsTab;
+  @FXML
+  private ListView<?> requirementsListView;
 
-  @FXML private TextArea requirementsTextArea;
+  @FXML
+  private ComboBox<?> projectSelectedComboBox;
 
-  @FXML private ComboBox<?> projectSelectedComboBox;
+  @FXML
+  private TextField requirementIDTextField;
 
-  @FXML private TextField requirementIDTextField;
+  @FXML
+  private ComboBox<?> requirementStatusComboBox;
 
-  @FXML private ComboBox<?> requirementStatusComboBox;
+  @FXML
+  private ComboBox<?> requirementTypeComboBox;
 
-  @FXML private ComboBox<?> requirementTypeComboBox;
+  @FXML
+  private TextField requirementDescriptionTextField;
 
-  @FXML private TextField requirementDescriptionTextField;
+  @FXML
+  private TextField estimateHoursTextField;
 
-  @FXML private TextField estimateHoursTextField;
+  @FXML
+  private TextField priorityNumberTextField;
 
-  @FXML private TextField priorityNumberTextField;
+  @FXML
+  private TextField deadlineTextField;
 
-  @FXML private TextField deadlineTextField;
+  @FXML
+  private ComboBox<?> responsibleTeamMemberComboBox;
 
-  @FXML private ComboBox<?> responsibleTeamMemberComboBox;
+  @FXML
+  private Button addRequirementButton;
 
-  @FXML private Button addRequirementButton;
+  @FXML
+  private Button editRequirementButton;
 
-  @FXML private Button editRequirementButton;
+  @FXML
+  private Button saveRequirementButton;
 
-  @FXML private Button saveRequirementButton;
+  @FXML
+  private Tab tasksTab;
 
-  @FXML private Tab tasksTab;
+  @FXML
+  private ListView<?> tasksListView;
 
-  @FXML private TextArea tasksTextArea;
+  @FXML
+  private ComboBox<?> projectSelectedOnTasksComboBox;
 
-  @FXML private ComboBox<?> projectSelectedOnTasksComboBox;
+  @FXML
+  private ComboBox<?> requirementSelectedComboBox;
 
-  @FXML private ComboBox<?> requirementSelectedComboBox;
+  @FXML
+  private TextField taskIDTextField;
 
-  @FXML private TextField taskIDTextField;
+  @FXML
+  private ComboBox<?> taskStatusComboBox;
 
-  @FXML private ComboBox<?> taskStatusComboBox;
+  @FXML
+  private TextField taskDescriptionTextArea;
 
-  @FXML private TextField taskDescriptionTextArea;
+  @FXML
+  private TextField taskEstimateTextField;
 
-  @FXML private TextField taskEstimateTextField;
+  @FXML
+  private TextField taskTimeUsedTextField;
 
-  @FXML private TextField taskTimeUsedTextField;
+  @FXML
+  private TextField taskDeadline;
 
-  @FXML private TextField taskDeadline;
+  @FXML
+  private ComboBox<?> taskTeamMembersComboBox;
 
-  @FXML private ComboBox<?> taskTeamMembersComboBox;
+  @FXML
+  private Button addTaskButton;
 
-  @FXML private Button addTaskButton;
+  @FXML
+  private Button editTaskButton;
 
-  @FXML private Button editTaskButton;
+  @FXML
+  private Button saveTaskButton;
 
-  @FXML private Button saveTaskButton;
+  @FXML
+  private Tab employeesTab;
 
-  @FXML private Tab employeesTab;
+  @FXML
+  private ListView<?> employeesListView;
 
-  @FXML private TextArea employeeTextArea;
+  @FXML
+  private TextField employeeIDTextField;
 
-  @FXML private TextField employeeIDTextField;
+  @FXML
+  private TextField employeeFirstName;
 
-  @FXML private TextField employeeFirstName;
+  @FXML
+  private TextField employeeLastName;
 
-  @FXML private TextField employeeLastName;
+  @FXML
+  private ComboBox<?> employeeRoleComboBox;
 
-  @FXML private ComboBox<?> employeeRoleComboBox;
+  @FXML
+  private TextArea employeeTaskTextArea;
 
-  @FXML private TextArea employeeTaskTextArea;
+  @FXML
+  private Button addEmployeeButton;
 
-  @FXML private Button addEmployeeButton;
+  @FXML
+  private Button editEmployeeButton;
 
-  @FXML private Button editEmployeeButton;
+  @FXML
+  private Button saveEmployeeButton;
 
-  @FXML private Button saveEmployeeButton;
 
   private SystemAdapter adapter;
   private ProjectManagmentSystem colourIT;
@@ -175,7 +226,19 @@ public class Controller
       }
       else
       {
-        adapter.editProject(newName, oldName);
+        try
+        {
+          adapter.editProject(newName, oldName);
+        }
+        catch (IllegalArgumentException er)
+        {
+          Alert alert = new Alert(Alert.AlertType.ERROR,
+              er.getMessage() , ButtonType.OK);
+          alert.setTitle("Overlapping data");
+          alert.setHeaderText(null);
+
+          alert.showAndWait();
+        }
       }
       updateProjects();
       projectNameTextField.setEditable(false);
